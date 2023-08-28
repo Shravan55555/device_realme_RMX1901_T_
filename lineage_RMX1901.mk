6f55a0502bc0ae94e3dea4b2d2debbed76d98c81
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-08-28 22:32:05 +0530

    Update and rename blaze_RMX1901.mk to lineage_RMX1901.mk

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,18 +10,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 # Inherit some common Rising stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1080
-BLAZE_BUILD_TYPE := UNOFFICIAL
-WITH_GAPPS := true
-TARGET_SUPPORTS_BLUR := true
+# maintainer flag (one word only/no spacing)
+ALPHA_MAINTAINER := SHRAVAN
+WITH_GAPPS := 1
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# UDFPS ICONS/ANIMATIONS
 TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+
+# Wether to use google (true) or AOSP (false) telephony package bundle. (defaults: false for gms core, true for pixel builds)
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,7 +40,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := blaze_RMX1901
+PRODUCT_NAME := lineage_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
@@ -41,8 +52,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="RMX1901"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ2A.230805.001/10316531:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
