2d29dda1f67ced7a004f66e98344e7c38a7c9c3f
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-08-28 22:29:00 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_RMX1901.mk
+    $(LOCAL_DIR)/lineage_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_RMX1901-userdebug \
-    blaze_RMX1901-eng
+    lineage_RMX1901-userdebug \
+    lineage_RMX1901-eng
 
